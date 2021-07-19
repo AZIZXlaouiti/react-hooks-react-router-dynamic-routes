bd94f19753565f7f8fc74604bf846a249c2c8368
AZIZXlaouiti <azizrocke@gmail.com>
2021-07-19 15:28:33 -0400

    Completed

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 export default function MoviesList({movies}) {
-    const renderMovies = Object.keys(movies).map((moviesId)=>(
-        <li key={moviesId}>
-            <Link to={`/movies/${moviesId}`}>{movies[moviesId].title} </Link>
+    const renderMovies = Object.keys(movies).map((movieId)=>(
+        <li key={movieId}>
+            <Link to={`/movies/${movieId}`}>{movies[movieId].title} </Link>
         </li>
     ));
     return <ul>{renderMovies}</ul>
